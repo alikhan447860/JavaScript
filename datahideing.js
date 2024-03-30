b3415e3ccf5dfe786bd7ddc2efe3a14ab333af56
Ali khan <112551290+alikhan447860@users.noreply.github.com>
2024-03-30 16:25:48 +0530

    js daily

--- datahideing.js
+++ datahideing.js
@@ -4,7 +4,6 @@ var count=0;
  count++;
  console.log(count);
 }
-inccount();
-
+return inccount;
 }
-counter();
+counter()();
---
+++ functions.js
@@ -0,0 +1,17 @@
+// function statement and function declaration
+function a(){
+
+    console.log("a called");
+}
+a();
+// function expression
+var b=function(){
+    console.log("b called");
+}
+b();
+
+
+function c(param1,param2){
+    console.log(param1,param2);
+}
+c(1,2);
